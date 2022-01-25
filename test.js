223c4157d5b2c5ba08376035300dc78d2b1e3ccd
infotechlingo <infotechlingo@gmail.com>
2022-01-25 21:52:48 +0300

    initial commit

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
- res.send('Hello InfoTechlingo')
+ res.send('Hello Virtual Machine!')
 })
 app.listen(port, () => {
  console.log(`Express Application listening at port 3000`)
