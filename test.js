15303f7b6a54c4714c498fd3487903d1f5177063
infotechlingo <infotechlingo@gmail.com>
2022-01-27 18:48:27 +0300

    3r change

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
- res.send('Hello Virtual Machine!')
+ res.send('Hello Virtual Machine222!')
 })
 app.listen(port, () => {
  console.log(`Express Application listening at port 3000`)
